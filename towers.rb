40cf4eb6da1fb5e722c24401ae1420f095c91768
yxlau <yixuan.lau@gmail.com>
2016-12-08 16:31:12 +0800

    Complete game

--- towers.rb
+++ towers.rb
@@ -4,7 +4,7 @@ class TowerOfHanoi
   end
 
   def play
-    towers = [ [ 'o', 'oo', 'ooo'], [' ', ' ', ' '], [' ', ' ', ' '] ]
+    towers = [['o', 'oo', 'ooo'], [' ', ' ', ' '], [' ', ' ', ' ']]
     game_over = false
     valid_move = false
     move = ''
@@ -27,58 +27,87 @@ class TowerOfHanoi
 
       end
 
-      puts "move validated"
-
-      # make move
+      # move validated, so make move
+      make_move(move, towers)
 
       #render towers again
+      render(towers)
 
       # check if user has won
-
-      # if has won, congratulate user and change game_over state
+      if won?(towers)
+        puts "Congratulations! You've won!"
+        exit
+      end
 
       # otherwise, reset valid_move
-
-
-      # prevent infinite loop
-      game_over = true
+      valid_move = false
 
     end
   end
 
+  def won?(towers)
+    towers.each_with_index do |tower, i|
+      return true if tower.none? { |slot| slot == ' '} && i != 0
+    end
+    return false
+  end
+
+  def make_move(move, towers)
+    current_tower = towers[move[0].to_i - 1]
+    new_tower = towers[move[-1].to_i - 1]
+
+    # get the top disc
+    disc = current_tower.find { |disc| disc != ' '}
+
+    # place disc in bottom-most slot of new tower if all empty
+    new_tower[-new_tower.reverse.index(' ') - 1] = disc
+
+    # make its former place an empty slot
+    current_tower[current_tower.index(disc)] = ' '
+
+  end
 
 
   def render(towers)
-    rungs = towers[0].size
-    towers.each_with_index do |tower, t|
-      (0...rungs).each do |r|
-        print towers[r][t].ljust(5)
+    (0...@towers).each do |tower|
+      (0...@towers).each do |row|
+        print towers[row][tower].ljust(5)
       end
       puts "\n"
     end
-    (1..@towers).to_a.each { |n| print n.to_s.ljust(5) }
+    (1..@towers).each { |n| print n.to_s.ljust(5)}
     puts "\n"
   end
 
   def validate_move(move, towers)
-    if move.length == 3
-      # convert to int and a
-      moves = move.split(',').map{ |tower| tower.to_i}
-
-      # make sure all towers exist
-      if moves.any? {|tower| tower < 1 || tower > @towers}
-        puts "Tower not found. Please move disc between our available towers: #{(1..@towers).to_a}"
-        return false
-      end
-
-      # make sure tower we're transferring from has disc
-      if towers[moves[0]-1].all? { |tower| tower == ' '}
-        puts "We couldn't find any discs to transfer. Try transfering discs from another tower"
-        return false
-      end
-    else
+    unless move.length == 3
       puts "You might have got your tower numbers or the format wrong.\n Enter where you'd like to move from and to in the format '1,3'."
       return false
+    end
+    # convert to int and a
+    moves = [move[0].to_i-1, move[-1].to_i-1]
+
+    # make sure all towers exist
+    if moves.any? {|tower_index| tower_index < 0 || tower_index >= @towers}
+      puts "Tower not found. Please move disc between our available towers: #{(1..@towers).to_a}"
+      return false
+    end
+
+    # make sure tower we're transferring from has disc
+    if towers[moves[0]].all? { |tower| tower == ' '}
+      puts "We couldn't find any discs to transfer. Try transfering discs from another tower"
+      return false
+    end
+
+    # if new tower has a disc, make sure it's not smaller than the disc we're trying to move
+    if towers[moves[1]].any? { |slot| slot != ' '}
+      disc = towers[moves[0]].find { |disc| disc != ' '}
+      if towers[moves[1]].any? { |item| item != ' ' && disc.size > item.size }
+        puts "No disk may be placed on top of a smaller disk"
+        render(towers)
+        return false
+      end
+
     end
 
     # validate move
